0ba93f9d91f47472a1ac3970adcea14e3120ba14
kgotlelelo-cmd <kgotlelelo-cmd>
2020-06-29 19:35:29 +0200

    playing with char's

---
+++ playing_with_characters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	char ch;
+	char s[500];
+	char sen[500];
+
+	scanf("%c",&ch);
+	scanf("%s",s);
+	scanf("\n");
+	scanf("%[^\n]%*c",sen);
+
+	printf("%c\n%s\n%s\n",ch,s,sen);
+	return 0;
+}
