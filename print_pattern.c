0a8f6d2362b3e842122ce638d3f9f212b590bdad
kgotlelelo-cmd <kgotlelelo-cmd>
2020-07-04 20:09:17 +0200

    discussion

---
+++ print_pattern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void print_pattern(int n)
+{
+	int len;
+
+	len = 2*n -1;
+	for(int i = 0;i<len;i++)
+	{
+		for(int j = 0;j<len;j++)
+		{
+			int min = i<j ? i : j;
+			min = min<len-i?min:len -i-1;
+			min = min<len-j-1?min:len-j-1;
+			printf("%d",n-min);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+	print_pattern(n);
+	return 0;
+}
